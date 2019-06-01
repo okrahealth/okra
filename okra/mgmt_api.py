64316258b481a7aa33f5f7bee33db1759bbc551b
tbonza <tylers.pile@gmail.com>
2019-05-31 20:34:23 -0400

    refactored to include yearmo options, see #14

--- okra/mgmt_api.py
+++ okra/mgmt_api.py
@@ -19,7 +19,10 @@ from okra.assn4 import (get_truck_factor_by_project,
                         total_number_of_contributors_by_project)
 from okra.models import DataAccessLayer, Author, Meta, Contrib, CommitFile
 from okra.proto import okra_api_pb2
-
+from okra.repository_metrics import (repo_info, iso_date_aggregation,
+                                     find_author_count,
+                                     find_contributor_count,
+                                     find_file_metrics)
 
 logger = logging.getLogger(__name__)
 
@@ -33,7 +36,7 @@ def msg_iso_date_aggregation(msg, item, status: str, yearmo: str):
     isodt.yearmo = yearmo
     isodt.commit_hash = item.commit_hash
 
-    isoyr, isowk, isody = item.authored.isocalendar()
+    isoyr, isowk, isody = item.contributed.isocalendar()
 
     isodt.iso_week = isowk
     isodt.iso_year = isoyr
@@ -52,11 +55,7 @@ def msg_repository_info(dal: DataAccessLayer, repo_id: str, yearmo: str):
 
     msg = okra_api_pb2.RepositoryInfo()
 
-    q = dal.session.query(
-        Meta.yearmo, Author.commit_hash, Author.authored
-    ).join(Author).filter(Meta.yearmo == yearmo)
-
-    qres = q.all()
+    qres = repo_info(dal, yearmo)
 
     if len(qres) == 0:
         msg.repo_id = repo_id
@@ -75,7 +74,6 @@ def msg_repository_info(dal: DataAccessLayer, repo_id: str, yearmo: str):
                                    yearmo=yearmo)
     msg = msg_iso_date_aggregation(msg, item=last, status='last',
                                    yearmo=yearmo)
-
     return msg
 
 def msg_repository_metric(dal: DataAccessLayer, repo_id: str, yearmo: str):
@@ -87,11 +85,7 @@ def msg_repository_metric(dal: DataAccessLayer, repo_id: str, yearmo: str):
 
     # IsoDataAggregation
 
-    q1 = dal.session.query(
-        Meta.commit_hash, Meta.yearmo, Author.authored
-    ).join(Author).filter(Meta.yearmo == yearmo)
-
-    qres1 = q1.all()
+    qres1 = iso_date_aggregation(dal, yearmo)
 
     if len(qres1) == 0:
         return msg
@@ -104,74 +98,18 @@ def msg_repository_metric(dal: DataAccessLayer, repo_id: str, yearmo: str):
 
     # Author count
 
-    q2 = dal.session.query(
-        Meta.commit_hash, Meta.yearmo, Author.name, Author.email
-    ).join(Author).filter(Meta.yearmo == yearmo)
-
-    msg.author_count = q2.count()
+    msg.author_count = find_author_count(dal, yearmo)
     
     # Contributor count
 
-    q3 = dal.session.query(
-        Meta.commit_hash, Meta.yearmo, Contrib.name, Contrib.email
-    ).join(Contrib).filter(Meta.yearmo == yearmo)
-
-    msg.contrib_count = q3.count()
+    msg.contrib_count = find_contributor_count(dal, yearmo)
 
     # File level metrics
 
-    q4 = dal.session.query(
-        Meta.yearmo,
-        func.count(CommitFile.modified_file).label('file_count'),
-        func.sum(CommitFile.lines_added).label('lines_added'),
-        func.sum(CommitFile.lines_subtracted).label('lines_subtracted')
-    ).join(CommitFile).filter(Meta.yearmo == yearmo).group_by(Meta.yearmo)
 
-    qres4 = q4.first() # Should only be one yearmo
+    qres4 = find_file_metrics(dal, yearmo)
     msg.file_count = qres4.file_count
     msg.lines_added = qres4.lines_added
     msg.lines_subtracted = qres4.lines_subtracted
 
     return msg
-
-def tbl_repository_metrics(dburl: str, rd: list, pqcache: str, name='repo_metrics'):
-    """ RepositoryMetrics table 
-
-    rating is the computed truck factor for a project.
-    """
-    try:
-        dal = DataAccessLayer(dburl)
-        dal.connect()
-        dal.session = dal.Session()
-
-        results = []
-        for item in rd:
-
-            repo_id = item['repo_id']
-            owner, project = repo_id.split('/')
-            truck_factor, _ = get_truck_factor_by_project(owner, project, dal)
-            total_number_of_files = total_number_of_files_by_project(owner, project, dal)
-            total_number_of_contributors = total_number_of_contributors_by_project(owner, project, dal)
-
-            results.append({
-                'repo_id': repo_id,
-                'rating': truck_factor,
-                'total_number_of_files': total_number_of_files,
-                'total_number_of_contributors': total_number_of_contributors
-            })
-
-        dal.session.close()
-        return results
-    
-    except Exception as exc:
-        logger.exception(exc)
-
-
-def tbl_contributor():
-    """ Contributor table """
-    pass
-
-# Contributor
-
-
-# Repository Info
