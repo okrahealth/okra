fc24553cc14ca426862dd11da4ae5d1bebf51326
tbonza <tylers.pile@gmail.com>
2019-07-04 19:51:22 -0400

    minor update to setup.py, see #16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='okra',
-    version='1.3.1',
+    version='1.3.2',
     packages=["okra", "okra/proto"],
     license='MIT',
     author="Tyler Brown",
