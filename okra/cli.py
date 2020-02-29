62499c587c45973a445b94b69aa700a1a9204574
tbonza <tylers.pile@gmail.com>
2020-02-28 21:56:53 -0500

    adding cli for populating db with repo commits

--- okra/cli.py
+++ okra/cli.py
@@ -1,10 +1,16 @@
 """ Command Line Interface utilities for Okra """
 import click
 
+from okra.populate_db import populate_db
+
 @click.group()
 def cli():
     pass
 
 @cli.command(name='persist')
-def download_update_repos():
-    pass
+@click.argument('dirpath')
+@click.argument('repo_name')
+@click.argument('dburl')
+@click.option('buffer_size', default=1e5, help="number of commits before db write")
+def clone_pull_repo(dirpath, repo_name, dburl, buffer_size):
+    populate_db(dburl, dirpath, repo_name, buffer_size)
